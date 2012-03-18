65b3d4849b36d9341c8eec640f6e3bfe3c21b24a
Mike Chandler <mike@rmwebfx.com>
2012-03-18 13:25:23 -0700

    Same thing, but using my API wrapper.

--- src/main/java/com/rmwebfx/citygridsearch/controller/Test.java
+++ src/main/java/com/rmwebfx/citygridsearch/controller/Test.java
@@ -4,34 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.citygrid.CGException;
-import com.citygrid.CityGrid;
-import com.citygrid.content.places.detail.CGPlacesDetail;
-import com.citygrid.content.places.detail.CGPlacesDetailLocation;
-import com.citygrid.content.places.detail.CGPlacesDetailResults;
-import com.rmwebfx.citygridsearch.config.Constants;
+import com.rmwebfx.citygridsearch.resources.Place;
 
 @Controller
 public class Test {
 	
 	@RequestMapping(value = "/")
 	public String home(Model model) {
-		CityGrid.setPublisher(Constants.CITYGRID_PUBLISHER_ID);
-		CityGrid.setSimulation(false);
-		CGPlacesDetail detail = CityGrid.placesDetail();
-		detail.setLocationId(664259190);
+		Place place = new Place(664259190);
 		
-		String locationName;
-		
-		try {
-			CGPlacesDetailResults results = detail.detail();
-			CGPlacesDetailLocation location = results.getLocation();
-			locationName = location.getName();
-		} catch (CGException e) {
-			// TODO: Handle failures
-			locationName = e.getMessage();
-		}
-		model.addAttribute("locationName", locationName);
+		model.addAttribute("locationName", place.getName());
 		return "home";
 	}
 	
