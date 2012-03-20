0a8b099b3dccb57b0d2ab7c9938e19de9c1c3e7c
Mike Chandler <mike@rmwebfx.com>
2012-03-19 19:31:24 -0700

    Modifying PlacesSearch to account for config options.:

--- src/main/java/com/rmwebfx/citygridsearch/config/Constants.java
+++ src/main/java/com/rmwebfx/citygridsearch/config/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static String CITYGRID_PUBLISHER_ID = "10000002304";
 	public static String SITEWIDE_CITY_STATE = "Simi Valley, CA";
-	
+	public static int SEARCH_RESULTS_PER_PAGE = 10;
 	
 	public static String publicIdString(String publicId) {
 		String cleanString = publicId.toLowerCase().replaceAll("[^A-Za-z0-9 ]", "");
--- src/main/java/com/rmwebfx/citygridsearch/resources/PlacesSearch.java
+++ src/main/java/com/rmwebfx/citygridsearch/resources/PlacesSearch.java
@@ -9,10 +9,14 @@ import com.citygrid.content.places.search.CGPlacesSearch;
 import com.citygrid.content.places.search.CGPlacesSearchLocation;
 import com.citygrid.content.places.search.CGPlacesSearchResults;
 import com.citygrid.content.places.search.CGPlacesSearchType;
+import com.rmwebfx.citygridsearch.config.Constants;
 
 public class PlacesSearch extends RemoteResource {
 	
 	private List<PlacesSearchMatch> matches;
+	private int totalHits;
+	private int page;
+	private int resultsPerPage;
 	
 	public PlacesSearch(String searchType, String where) {
 		super();
@@ -20,9 +24,14 @@ public class PlacesSearch extends RemoteResource {
 		CGPlacesSearch search = CityGrid.placesSearch();
 	    search.setType(CGPlacesSearchType.Restaurant); // TODO: Handle search types
 	    search.setWhere(where);
+	    search.setResultsPerPage(Constants.SEARCH_RESULTS_PER_PAGE);
 	    
 	    try {
 	        CGPlacesSearchResults results = search.search();
+	        totalHits = results.getTotalHits();
+	        page = results.getPage();
+	        resultsPerPage = results.getResultsPerPage();
+	        
 	        CGPlacesSearchLocation[] locations = results.getLocations();
 	        setPlaces(locations);
 	    }
@@ -45,4 +54,15 @@ public class PlacesSearch extends RemoteResource {
 		return matches;
 	}
 	
+	public int getTotalHits() {
+		return totalHits;
+	}
+	
+	public int getPage() {
+		return page;
+	}
+	
+	public int getResultsPerPage() {
+		return resultsPerPage;
+	}
 }
