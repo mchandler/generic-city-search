88e75cc28ab4e6747399d0ac82019cd85163fc25
Mike Chandler <mike@rmwebfx.com>
2012-03-25 10:41:06 -0700

    Still working on it...

--- src/main/java/com/rmwebfx/citygridsearch/resources/PlaceReviews.java
+++ src/main/java/com/rmwebfx/citygridsearch/resources/PlaceReviews.java
@@ -43,4 +43,8 @@ public class PlaceReviews extends RemoteResource {
 		}
 	}
 	
+	public List<SearchReview> getReviews() {
+		return reviews;
+	}
+	
 }
--- src/main/java/com/rmwebfx/citygridsearch/ws/ReviewResource.java
+++ src/main/java/com/rmwebfx/citygridsearch/ws/ReviewResource.java
@@ -2,15 +2,34 @@ package com.rmwebfx.citygridsearch.ws;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
+
+import com.rmwebfx.citygridsearch.representation.SearchReviewsRepresentation;
+import com.rmwebfx.citygridsearch.resources.PlaceReviews;
 
 @Path("/reviews")
 public class ReviewResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String reviewsByLocation() {
-		return "{\"reviews\":\"placeholder\"}";
+	@Path("{locationId}/{page}")
+	public String reviewsByLocation(@PathParam("locationId") int locationId,
+			@PathParam("page") int page) {
+		
+		String jsonResponse;
+		
+		PlaceReviews review = new PlaceReviews(44544412, page);
+		try {
+			String json = SearchReviewsRepresentation.represents(review.getReviews());
+			jsonResponse = json;
+		} catch (JSONException e) {
+			jsonResponse = e.getMessage();
+		}
+		
+		return jsonResponse;
 	}
 }
