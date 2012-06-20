b7aa941bfde0966c8375c463d8dc11b052500fd7
Mike Chandler <mike@rmwebfx.com>
2012-06-19 20:38:15 -0700

    Removed unused import statement.

--- src/main/java/com/rmwebfx/citygridsearch/controller/SearchController.java
+++ src/main/java/com/rmwebfx/citygridsearch/controller/SearchController.java
@@ -3,7 +3,6 @@ package com.rmwebfx.citygridsearch.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rmwebfx.citygridsearch.config.Constants;
