198b1dcae6634f137ca27b9d9597e4678babac0a
Mike Chandler <mike@rmwebfx.com>
2012-12-21 20:34:21 -0800

    Working on layout properties.

--- src/main/java/com/rmwebfx/citygridsearch/core/BasePage.java
+++ src/main/java/com/rmwebfx/citygridsearch/core/BasePage.java
@@ -1,9 +1,14 @@
 package com.rmwebfx.citygridsearch.core;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 
 public class BasePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
+	
+	public BasePage() {
+		add(new WebMarkupContainer("stylecss"));
+	}
 
 }
