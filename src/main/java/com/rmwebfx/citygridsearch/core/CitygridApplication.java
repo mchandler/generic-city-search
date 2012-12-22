338ed6f00250528760256e640266a5c575a3e4aa
Mike Chandler <mike@rmwebfx.com>
2012-12-22 12:28:43 -0800

    Mounting page URLs

--- src/main/java/com/rmwebfx/citygridsearch/core/CitygridApplication.java
+++ src/main/java/com/rmwebfx/citygridsearch/core/CitygridApplication.java
@@ -4,12 +4,26 @@ import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 import com.rmwebfx.citygridsearch.page.Index;
+import com.rmwebfx.citygridsearch.page.CategoryPage;
+import com.rmwebfx.citygridsearch.page.PlaceDetails;
 
 public class CitygridApplication extends WebApplication {
 
 	@Override
 	public Class<? extends Page> getHomePage() {
 		return Index.class;
+	}
+	
+	@Override
+	public void init() {
+		super.init();
+		
+		mountPages();
+	}
+	
+	private void mountPages() {
+		mountPage("/search/${category}/${pageNumber}", CategoryPage.class);
+		mountPage("/place/${locationId}/${publicId}", PlaceDetails.class);
 	}
 
 }
