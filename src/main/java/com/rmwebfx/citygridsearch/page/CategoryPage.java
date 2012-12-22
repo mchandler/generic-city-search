96bc22c8a942d6d3e9c3ef6c1b441cac9d9e1f67
Mike Chandler <mike@rmwebfx.com>
2012-12-21 19:51:12 -0800

    Modified CategoryPage to hand links off to details page.

--- src/main/java/com/rmwebfx/citygridsearch/page/CategoryPage.java
+++ src/main/java/com/rmwebfx/citygridsearch/page/CategoryPage.java
@@ -1,11 +1,9 @@
 package com.rmwebfx.citygridsearch.page;
 
-import java.util.List;
-
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PageableListView;
-import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -22,16 +20,12 @@ public class CategoryPage extends BasePage {
 	private int categoryId = 1;
 	
 	public CategoryPage(PageParameters params) {
-		//pageNumber = params.get("pageNumber").toInt();
-		//categoryId = CategoryHelper.getCategory(params.get("category").toString());
-		
-		System.out.println("pageNumber: " + params.get("pageNumber").toString());
-		System.out.println("category: " + params.get("category").toString());
+		pageNumber = params.get("pageNumber").toInt();
+		categoryId = CategoryHelper.getCategory(params.get("category").toString());
 		
 		LoadableDetachableModel<PlacesSearch> places = loadPlaces();
-		LoadableDetachableModel<List<PlacesSearchMatch>> matches = getMatches(places);
 		
-		PageableListView listView = loadListView(matches);
+		PageableListView listView = loadListView(places);
 		
 		add(listView);
 		
@@ -52,28 +46,22 @@ public class CategoryPage extends BasePage {
 		return places;
 	}
 	
-	private LoadableDetachableModel<List<PlacesSearchMatch>> getMatches(final IModel places) {
-		LoadableDetachableModel<List<PlacesSearchMatch>> matches = new LoadableDetachableModel<List<PlacesSearchMatch>>() {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			protected List<PlacesSearchMatch> load() {
-				return ((PlacesSearch) places.getObject()).getPlaces();
-			}
-			
-		};
-		return matches;
-	}
-	
-	private PageableListView loadListView(LoadableDetachableModel<List<PlacesSearchMatch>> matches) {
+	private PageableListView loadListView(LoadableDetachableModel<PlacesSearch> places) {
 		@SuppressWarnings("unchecked")
-		PageableListView<PlacesSearchMatch> results = new PageableListView("results", matches, 10) {
+		PageableListView<PlacesSearchMatch> results = new PageableListView("results", places.getObject().getPlaces(), 10) {
 
 			@Override
 			protected void populateItem(ListItem item) {
 				PlacesSearchMatch match = (PlacesSearchMatch) item.getModelObject();
-				item.add(new Label("placeName",match.getName()));
+				
+				PageParameters params = new PageParameters();
+				params.add("locationId", match.getLocationId());
+				params.add("publicId", match.getPublicId());
+				BookmarkablePageLink placeLink = new BookmarkablePageLink("placeLink", PlaceDetails.class, params);
+				
+				placeLink.add(new Label("placeName", match.getName()));
+				
+				item.add(placeLink);
 				item.add(new Label("categories",match.getCategories()));
 				item.add(new Label("address", match.getAddress().toString()));
 			}
