a026798cf0cffabff6bc2221fc41a711613f388c
Mike Chandler <michael.chandler@onassignment.com>
2012-12-27 09:26:26 -0800

    Results count per page modified.

--- src/main/java/com/rmwebfx/citygridsearch/config/Constants.java
+++ src/main/java/com/rmwebfx/citygridsearch/config/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static String CITYGRID_PUBLISHER_ID = "10000002304";
 	public static String SITEWIDE_CITY_STATE = "Simi Valley, CA";
-	public static int SEARCH_RESULTS_PER_PAGE = 10;
+	public static int SEARCH_RESULTS_PER_PAGE = 8;
 	public static int REVIEWS_COUNT_PER_PLACE = 20;
 	
 }
